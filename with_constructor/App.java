20faef13998fd6dc0ddfbd621fb0bcf6283755ca
Francyne Rigor <111488342+cynrgr@users.noreply.github.com>
2022-08-25 15:38:50 +0800

    Create App.java

---
+++ with_constructor/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+        // Calls the sayMyName method
+        knight.sayMyName();
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        // Calls the sayMyName method
+        mage.sayMyName();
+
+        // Creates a new object named "mage" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        // Calls the sayMyName method
+        thief.sayMyName();
+
+        // Creates a new object named "mage" from Character class
+       Character archer = new Character(5, 7, 9, "Venti");
+       // Calls the sayMyName method
+       archer.sayMyName();
+
+       // Creates a new object named "mage" from Character class
+       Character healer = new Character(5, 10, 5, "Barbara");
+       // Calls the sayMyName method
+       healer.sayMyName();
+    }
+}
